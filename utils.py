217a67548c1d7ba83539e3fd9036037afcbd43ac
Georg Groenendaal <georg.groenendaal@gmail.com>
2020-12-29 14:49:53 +0100

    Change windowing feature to support complete sample, and uses faster np.concatenate

--- utils.py
+++ utils.py
@@ -1,13 +1,31 @@
 from typing import List, Tuple
 import numpy as np
+from IPython.display import clear_output
 
-def create_window(input_data: np.array, step_size: int) -> np.array:
+
+def create_window(input_data: np.array, window_size: int) -> np.array:
     data_len = len(input_data)
-    result = np.zeros((data_len-step_size+1, step_size, *input_data.shape[1:]))
+    result = np.zeros((data_len-window_size+1, window_size, *input_data.shape[1:]))
     for i in range(data_len):
-        if i+step_size <= data_len:
-            result[i] = input_data[i:i+step_size]
+        if i+window_size <= data_len:
+            result[i] = input_data[i:i+window_size]
     return result
+
+
+def create_window_on_multiple_samples(input_data: np.array, window_size: int) -> np.array:
+    """
+    Similar to create_window, but now can take multiple samples, will output in one
+    giant windowed np.array.
+    """
+    windowed_data = []
+    for i, sample in enumerate(input_data):
+        windowed_data.append(create_window(sample, window_size))
+
+        if i % 10000 == 0:
+            clear_output(wait=True)
+            print(f"Now at {i}")
+
+    return np.concatenate(windowed_data)
 
 
 def split_train_test(input_data: List) -> Tuple[np.array, np.array]:
